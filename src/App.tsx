625259d33ee4d6196cf6ca7742e10d78f9a069ca
divineion <najima.guermoudi@gmail.com>
2025-11-03 22:36:40 +0100

    feat(auth): include Login component to App
    Add Route "/" to Login component.
    On App render, retrieve token from local storage and update user global state properties `isLoggedIn` and `token`.

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,27 @@ import './App.css'
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Patients from "./features/patient/Patients";
 import Patient from "./features/patient/Patient.tsx";
+import Login from "./features/login/Login.tsx";
+import {useEffect} from "react";
+import type {AppDispatch} from "./app/store.ts";
+import {useDispatch} from "react-redux";
+import {setIsLoggedIn, setToken} from "./features/login/userSlice.ts";
 
 // je définis le routing url => component
 const App = () => {
+    const dispatch = useDispatch<AppDispatch>()
+
+    useEffect(() => {
+        const token = localStorage.getItem("authToken");
+        if (token) {
+            dispatch(setToken(token));
+            dispatch(setIsLoggedIn(true));
+        }
+    }, [dispatch]);
     return (
         <BrowserRouter>
             <Routes>
+                <Route path="/" element={<Login/>}/>
                 <Route path="/patients" element={<Patients/>}/>
                 <Route path="/patient/:uuid" element={<Patient />} />
             </Routes>
