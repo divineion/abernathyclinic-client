a91af76f2a7c570b08c9f2660e4eb1073ae11387
divineion <najima.guermoudi@gmail.com>
2025-11-04 23:08:24 +0100

    feat(logout): add temporary navbar with logout button
    Button.tsx: create reusable component to enforce using essential accessibility attributesvia props.
    Navbar.tsx: render a simple HTML <nav> element containing a `Button` child component, configured for logout and redirection to login page.

---
+++ src/common/components/Navbar.tsx
@@ -0,0 +1,24 @@
+import Button from "./Button.tsx";
+import {logout} from "../../features/login/loginThunk.ts";
+import {useDispatch} from "react-redux";
+import type {AppDispatch} from "../../app/store.ts";
+import {useNavigate} from "react-router-dom";
+
+const Navbar = () => {
+    const dispatch = useDispatch<AppDispatch>()
+    const navigate = useNavigate();
+    const handleLogoutButtonClick = () => {
+        dispatch(logout())
+        navigate("/")
+    }
+    return (
+        <>
+            <nav className={"container"}>
+                <Button handleClick={handleLogoutButtonClick} value={"x"} ariaLabel={"déconnexion"}
+                        title={"déconnexion"} className={"btn btn-primary"}/>
+            </nav>
+        </>
+    )
+}
+
+export default Navbar
