a8f581ae33a35c865f86ccda2998befd4c4f1f58
divineion <najima.guermoudi@gmail.com>
2025-11-04 21:00:53 +0100

    feat(login): add route "/user"
    Update `loginCheck()` to call API Gateway endpoint "/user" that returns the current user's role and username.

--- src/services/login.ts
+++ src/services/login.ts
@@ -1,7 +1,8 @@
 import api from "./axiosConfig.ts";
+import {GET_USER_INFO_ROUTE} from "../utils/apiRoutes.ts";
 
 export const loginCheck = async (username: string, password: string) => {
-    return await api.get("http://localhost:8080/patients", {
+    return await api.get(GET_USER_INFO_ROUTE, {
         auth: {
             username: username,
             password: password
