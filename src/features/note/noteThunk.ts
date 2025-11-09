299b5b973467cbc72723ff871e8d3a56f15f2e71
divineion <najima.guermoudi@gmail.com>
2025-11-09 15:27:19 +0100

    feat(notes): add noteThunk
    Add methods to call service and update NoteState or dispatch a feedback via the toast state.

---
+++ src/features/note/noteThunk.ts
@@ -0,0 +1,56 @@
+import {getNoteById, getNotesByDoctorId, getNotesByPatientUuid, updateNoteById} from "../../services/note.ts";
+import type {AppDispatch} from "../../app/store.ts";
+import {setFilteredNotes, setNote, setNotes} from "./noteSlice.ts";
+import type {UpdateNote} from "./types.ts";
+import {setToast} from "../snackbar/toastSlice.ts";
+import {isAxiosError} from "axios";
+
+export const getNotesByPatient = (uuid: string) => async (dispatch: AppDispatch) => {
+    try {
+        const data = await getNotesByPatientUuid(uuid);
+        dispatch(setNotes(data))
+    } catch (error: unknown) {
+        if (isAxiosError(error)) {
+            dispatch(setToast({open: true, variant: "error", message: error.message}))
+        }
+    }
+}
+
+export const getNotesByDoctor = (id: string) => async (dispatch: AppDispatch) => {
+    try {
+        const data = await getNotesByDoctorId(id)
+        dispatch(setFilteredNotes(data))
+    } catch (error: unknown) {
+        if (isAxiosError(error)) {
+            dispatch(setToast({open: true, variant: "error", message: error.message}))
+        }
+    }
+}
+
+export const getNote = (id: string) => async (dispatch: AppDispatch) => {
+    try {
+        const data = await getNoteById(id);
+        if (data) {
+            dispatch(setNote(data))
+        }
+    } catch (error: unknown) {
+        if (isAxiosError(error)) {
+            dispatch(setToast({open: true, variant: "error", message: error.message}))
+        }
+    }
+}
+
+export const updateNote = (id: string, note: UpdateNote) => async (dispatch: AppDispatch) => {
+    try {
+         const response = await updateNoteById(id, note)
+        if (!response?.success) {
+            dispatch(setToast({open: true, message: "une erreur est survenue pendant la mise à jour", variant:"error"}))
+        }
+
+        dispatch(setToast({open: true, message: "La mise à jour a été effectuée", variant:"success"}))
+    } catch (error: unknown) {
+        if (isAxiosError(error)) {
+            dispatch(setToast({open: true, message: error.message, variant:"error"}))
+        }
+    }
+}
