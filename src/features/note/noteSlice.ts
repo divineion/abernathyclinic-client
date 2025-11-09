89cab7e9a458b088932374208a07504f4e550292
divineion <najima.guermoudi@gmail.com>
2025-11-09 15:25:10 +0100

    feat(state): add note slice
    Add and export NoteState interface with properties:
     - `selectedNote`: Note ,
     - `notes` MinimalNote[],
     - `filter`: boolean,
     - `filteredNotes`: MinimalNote[].
    
    Add and export reducers for each property.

---
+++ src/features/note/noteSlice.ts
@@ -0,0 +1,39 @@
+import {createSlice, type PayloadAction} from "@reduxjs/toolkit"
+import type {MinimalNote, Note} from "./types.ts";
+
+interface NoteState {
+    selectedNote: Note | null,
+    notes: MinimalNote[],
+    filter: boolean,
+    filteredNotes: MinimalNote[],
+}
+
+ const initialState: NoteState = {
+    selectedNote: null,
+    notes: [],
+    filter: false,
+    filteredNotes: [],
+}
+
+export const noteSlice = createSlice({
+    name: "notes",
+    initialState,
+    reducers: {
+        setNote: (state, action: PayloadAction<Note>) => {
+          state.selectedNote = action.payload
+        },
+        setNotes: (state, action: PayloadAction<MinimalNote[]>)=> {
+            state.notes = action.payload
+        },
+        setFilter: (state, action: PayloadAction<boolean>) => {
+            state.filter = action.payload
+        },
+        setFilteredNotes: (state, action: PayloadAction<MinimalNote[]>) => {
+            state.filteredNotes = action.payload
+        }
+    }
+})
+
+export const { setNote, setNotes, setFilter, setFilteredNotes } = noteSlice.actions
+
+export default noteSlice.reducer
